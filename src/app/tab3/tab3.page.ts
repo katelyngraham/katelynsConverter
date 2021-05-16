05f6631beb67885cacdaf3b8e87dd9ac90fd3aa2
Katelyn <katelynxgraham@gmail.com>
2021-05-16 15:15:38 +0100

    convertion

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NavController} from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  amount=1;
+  from="euro";
+  to="usd";
+  warning="";
+  rates;
+  
+  constructor(public navCtrl: NavController) 
+  {
+    this.loadRates();
+  }
 
-  constructor() {}
+  convert()
+  {
+    console.log("amount =" + this.amount);
+    console.log("from = " + this.from);
+    console.log("to = " + this.to);
 
+    this.warning=this.from + " " + this.to;
+    if (this.from == this.to)
+    {
+      this.warning="Cannot have same from and to currency";
+      return;
+    }
+    //this.loadRates();
+    var rate = this.rates[this.from][this.to];
+    console.log("using rate "+ rate);
+    var convertedAmount = rate * this.amount;
+    console.log("converted amount = " + convertedAmount);
+    
+    
+    this.navCtrl.navigateForward('/tabs/tab2?amount=' + convertedAmount);
+  
+  }
+  loadRates(): any {
+    fetch('assets/rates.json').then(async res => {
+      this.rates = await res.json();
+    });
+    //this.http.get('assets/rates.json').map(res => res.json()).subscribe(res => {
+      //this.rates = res.menuItems;
+      //console.log(this.rates);
+      //return;
+    //},
+    //(err) => {
+    //  alert('failed loading json data');
+    //});
+  }
 }
+
